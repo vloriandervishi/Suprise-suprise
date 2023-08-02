8235f451041a4fe516705898d7ba5faa954a9761
vlorian <vlorian.dervishi@gmail.com>
2023-08-02 11:00:35 -0400

    addListenevent up,down,left,right

--- main.js
+++ main.js
@@ -8,24 +8,76 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((data) => renderQuotes(data)); //Send JSON data into a callback function
   }
   let count = 0;
+
   async function renderQuotes(data) {
     //Build the function that creates and appends elements to DOM
-    let quoteContainer = document.getElementById("quote-container");
-    quoteContainer.innerHTML = "";
+    let quoteContainer = document.getElementById("stage");
+    //quoteContainer.innerHTML = "";
+    let quoteElement = document.createElement("div");
 
-    document.addEventListener("click", (event) => {
+    document.addEventListener("click", () => {
+      // count will be updated to the number of characters
       data.results.splice(0, count).forEach((quote) => {
-        let quoteElement = document.createElement("div");
-        quoteElement.className = "card";
-        //quoteElement.style.width = "10rem ";
-        quoteElement.className = "grid-item";
-        quoteElement.innerHTML = `<img class="card-img-top" src=${quote.image}> <h2>${quote.name}</h2>`;
+        // div is created from DOM
+        //bootstrap styling with class name
+        // quoteElement.className = "card";
+        // properly align with grid class name
+        //quoteElement.className = "grid-item";
+        // will render the chards in the screen
+        quoteElement.id = "divImg";
+        quoteElement.innerHTML = `<img src=${quote.image}> <h2>${quote.name}</h2>`;
+
         quoteContainer.appendChild(quoteElement);
       });
-
-      return count++;
+      let mvLEFT = 0;
+      let mvTop = 0;
+      function moveStageLeft() {
+        let divImgLeft = document.getElementById("divImg");
+        divImgLeft.style.left = `${mvLEFT}px`;
+        if (mvLEFT >= 295) {
+          return (mvLEFT = 0);
+        }
+        return (mvLEFT += 5);
+      }
+      function moveStageRight() {
+        let divImgRight = document.getElementById("divImg");
+        divImgRight.style.left = `${mvLEFT}px`;
+        if (mvLEFT <= 0) {
+          return (mvLEFT = 0);
+        }
+        return (mvLEFT -= 5);
+      }
+      function moveStageUp() {
+        let divImgTop = document.getElementById("divImg");
+        divImgTop.style.top = `${mvTop}px`;
+        if (mvTop >= 400) {
+          return (mvTop = 0);
+        }
+        return (mvTop += 5);
+      }
+      function moveStageDown() {
+        let divImgDown = document.getElementById("divImg");
+        divImgDown.style.top = `${mvTop}px`;
+        if (mvTop <= 0) {
+          return (mvTop = 0);
+        }
+        return (mvTop -= 5);
+      }
+      document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+          moveStageLeft();
+        } else if (e.key === "ArrowRight") {
+          moveStageRight();
+        } else if (e.key === "ArrowUp") {
+          moveStageUp();
+        } else if (e.key === "ArrowDown") {
+          moveStageDown();
+        }
+      });
     });
+    return count++; //counts each time page is clicked
   }
 
+  // invoke function fetchData to render from api json list
   fetchData();
 });
