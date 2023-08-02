2455fa165bdf120d98ba0ffbcf4a2184c69e2625
vlorian <vlorian.dervishi@gmail.com>
2023-08-02 12:52:15 -0400

    added  comments

--- main.js
+++ main.js
@@ -90,5 +90,10 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // invoke function fetchData to render from api json list
+  // A callback is a function passed as an argument to another function
+
+  // This technique allows a function to call another function
+
+  // A callback function can run after another function has finished
   fetchData();
 });
