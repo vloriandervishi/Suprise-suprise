6d6d379da36291f2bbcbd779400d756f9c81cf40
vlorian <vlorian.dervishi@gmail.com>
2023-08-29 14:51:29 -0400

    refactored code

--- main.js
+++ main.js
@@ -1,89 +1,88 @@
-document.addEventListener("DOMContentLoaded", () => {
-  async function fetchData() {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+let stage = document.getElementById("stage");
+stage.className = "card";
+let divEl = document.createElement("div");
+let count = 0;
 
-    const data = await response.json();
+async function fetchData() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
 
-    return renderCharacters(data);
-  }
+  const data = await response.json();
 
-  let count = 0;
-  async function renderCharacters(data) {
-    let stage = document.getElementById("stage");
-    stage.className = "card";
-    let divEl = document.createElement("div");
-    //----------------------------------------------------------------
-    document.addEventListener("click", () => {
-      data.results.splice(0, count).forEach((quote) => {
-        divEl.id = "divImg";
+  return renderCharacters(data);
+}
 
-        divEl.innerHTML = `<img class="card-img-top" style="width:10rem;" src=${quote.image}> <h6>${quote.name}</h6><p>${quote.status}</p>`;
+function renderCharacters(data) {
+  //----------------------------------------------------------------
+  document.addEventListener("click", () => {
+    data.results.splice(0, count).forEach((quote) => {
+      divEl.id = "divImg";
 
-        stage.appendChild(divEl);
-      });
-      //----------------------------------------------------------------
-      let mvLEFT = 0;
-      let mvTop = 0;
+      divEl.innerHTML = `<img class="card-img-top" style="width:10rem;" src=${quote.image}> <h6>${quote.name}</h6><p>${quote.status}</p>`;
 
-      //--------------------------------------------------------
-      function moveStageLeft() {
-        let divImgLeft = document.getElementById("divImg");
-        divImgLeft.style.left = `${mvLEFT}px`;
-        if (mvLEFT >= 421) {
-          return (mvLEFT = 0);
-        }
-        return (mvLEFT += 10.5);
-      }
-      // ----------------------------------------------------------------
-      function moveStageRight() {
-        let divImgRight = document.getElementById("divImg");
-        divImgRight.style.left = `${mvLEFT}px`;
-
-        if (mvLEFT <= 0) {
-          return (mvLEFT = 0);
-        }
-        return (mvLEFT -= 10.5);
-      }
-      //----------------------------------------------------------------
-      function moveStageUp() {
-        let divImgTop = document.getElementById("divImg");
-        divImgTop.style.top = `${mvTop}px`;
-        if (mvTop >= 367) {
-          return (mvTop = 0);
-        }
-        return (mvTop += 10.5);
-      }
-      //----------------------------------------------------------------
-      function moveStageDown() {
-        let divImgDown = document.getElementById("divImg");
-        divImgDown.style.top = `${mvTop}px`;
-        if (mvTop <= 0) {
-          return (mvTop = 0);
-        }
-        return (mvTop -= 10.5);
-      }
-      document.addEventListener("keydown", function (e) {
-        if (e.key === "ArrowLeft") {
-          moveStageRight();
-        } else if (e.key === "ArrowRight") {
-          moveStageLeft();
-        } else if (e.key === "ArrowUp") {
-          moveStageDown();
-        } else if (e.key === "ArrowDown") {
-          moveStageUp();
-        }
-      });
-      let h6Styles = document.querySelector("h6");
-      let pStyles = document.querySelector("p");
-
-      document.addEventListener("mouseover", function (e) {
-        e.preventDefault();
-        h6Styles.style.color = "red";
-        pStyles.style.color = "violet";
-      });
+      stage.appendChild(divEl);
     });
-    return count++;
-  }
+    //----------------------------------------------------------------
+    let mvLEFT = 0;
+    let mvTop = 0;
 
-  fetchData();
-});
+    //--------------------------------------------------------
+    function moveStageLeft() {
+      let divImgLeft = document.getElementById("divImg");
+      divImgLeft.style.left = `${mvLEFT}px`;
+      if (mvLEFT >= 421) {
+        return (mvLEFT = 0);
+      }
+      return (mvLEFT += 10.5);
+    }
+    // ----------------------------------------------------------------
+    function moveStageRight() {
+      let divImgRight = document.getElementById("divImg");
+      divImgRight.style.left = `${mvLEFT}px`;
+
+      if (mvLEFT <= 0) {
+        return (mvLEFT = 0);
+      }
+      return (mvLEFT -= 10.5);
+    }
+    //----------------------------------------------------------------
+    function moveStageUp() {
+      let divImgTop = document.getElementById("divImg");
+      divImgTop.style.top = `${mvTop}px`;
+      if (mvTop >= 367) {
+        return (mvTop = 0);
+      }
+      return (mvTop += 10.5);
+    }
+    //----------------------------------------------------------------
+    function moveStageDown() {
+      let divImgDown = document.getElementById("divImg");
+      divImgDown.style.top = `${mvTop}px`;
+      if (mvTop <= 0) {
+        return (mvTop = 0);
+      }
+      return (mvTop -= 10.5);
+    }
+    document.addEventListener("keydown", function (e) {
+      if (e.key === "ArrowLeft") {
+        moveStageRight();
+      } else if (e.key === "ArrowRight") {
+        moveStageLeft();
+      } else if (e.key === "ArrowUp") {
+        moveStageDown();
+      } else if (e.key === "ArrowDown") {
+        moveStageUp();
+      }
+    });
+    let h6Styles = document.querySelector("h6");
+    let pStyles = document.querySelector("p");
+
+    document.addEventListener("mouseover", function (e) {
+      e.preventDefault();
+      h6Styles.style.color = "red";
+      pStyles.style.color = "violet";
+    });
+  });
+  return count++;
+}
+
+document.addEventListener("DOMContentLoaded", fetchData);
