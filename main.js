5b3672c7330a28f7b7561cbe0dd346c1408ad8a9
vlorian <vlorian.dervishi@gmail.com>
2023-08-29 15:17:16 -0400

    DRY some more code

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ stage.className = "card";
 let divEl = document.createElement("div");
 let mvLEFT = 0;
 let mvTop = 0;
+let divImgLeft,
+  divImgRight,
+  divImgTop,
+  divImgDown = document.getElementById("divImg");
+
 async function fetchData() {
   const response = await fetch("https://rickandmortyapi.com/api/character");
 
   const data = await response.json();
 
-  return renderCharacters(data);
+  renderCharacters(data);
 }
 //--------------------------------------------------------
 function moveStageLeft() {
-  let divImgLeft = document.getElementById("divImg");
   divImgLeft.style.left = `${mvLEFT}px`;
   if (mvLEFT >= 421) {
     return (mvLEFT = 0);
@@ -21,7 +25,6 @@ function moveStageLeft() {
 }
 // ----------------------------------------------------------------
 function moveStageRight() {
-  let divImgRight = document.getElementById("divImg");
   divImgRight.style.left = `${mvLEFT}px`;
 
   if (mvLEFT <= 0) {
@@ -31,7 +34,6 @@ function moveStageRight() {
 }
 //----------------------------------------------------------------
 function moveStageUp() {
-  let divImgTop = document.getElementById("divImg");
   divImgTop.style.top = `${mvTop}px`;
   if (mvTop >= 367) {
     return (mvTop = 0);
@@ -40,7 +42,6 @@ function moveStageUp() {
 }
 //----------------------------------------------------------------
 function moveStageDown() {
-  let divImgDown = document.getElementById("divImg");
   divImgDown.style.top = `${mvTop}px`;
   if (mvTop <= 0) {
     return (mvTop = 0);
@@ -55,7 +56,7 @@ function renderCharacters(data) {
 
       divEl.innerHTML = `<img class="card-img-top" style="width:10rem;" src=${quote.image}> <h6>${quote.name}</h6><p>${quote.status}</p>`;
 
-      stage.appendChild(divEl);
+      stage.append(divEl);
     });
     //----------------------------------------------------------------
 
